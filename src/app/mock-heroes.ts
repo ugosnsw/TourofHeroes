aa7cac6a9e4516337d39e580b45aa01faf4f1d6c
unknown <ugo.anekwe@service.nsw.gov.au>
2018-04-24 09:33:26 +1000

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: "Wolverine"},
+    {id: 2, name: "Cyclops"},
+    {id: 3, name: "Jean Grey"},
+    {id: 4, name: "Magento"},
+    {id: 5, name: "Jubilee"},
+    {id: 6, name: "Storm"},
+    {id: 7, name: "Gambit"},
+    {id: 5, name: "Nightcrawler"},
+    {id: 6, name: "Surge"},
+    {id: 7, name: "Firestorm"},
+    {id: 5, name: "Kangol"},
+    {id: 6, name: "Colossus"},
+    {id: 7, name: "Freud"},
+]
